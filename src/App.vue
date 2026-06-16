2b56aec396302c0487416bafed006e1ab6f8f7fa
momopig <121978614@qq.com>
2026-06-17 03:25:03 +0800

    fix: 删除无用代码

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,11 @@
   </div>
 </template>
 <script setup>
-import Home from './views/Home/index.vue';
 import { ref, computed, watch, onMounted } from 'vue';
 import './css/common.less';
-import{ getLocal } from '@/utils/storage';
+import { getLocal } from '@/utils/storage';
 
-onMounted(async () => {
-
-});
+onMounted(async () => {});
 </script>
 <style scoped lang="less">
 .shop-list {
